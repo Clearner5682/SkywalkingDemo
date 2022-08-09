11a4db7dcf8055edb59f6e03515ed34c3c080136
hongyan <625293627@qq.com>
2022-08-09 20:46:16 +0800

    2

---
+++ apply.sh
@@ -0,0 +1,5 @@
+kubectl apply -f authserver.yml
+kubectl apply -f base-setting-service.yml
+kubectl apply -f gateway.yml
+kubectl apply -f identity-service.yml
+kubectl apply -f process-control-service.yml
