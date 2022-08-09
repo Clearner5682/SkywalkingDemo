e2f37c66b7bfd8fdfbb30da035b0775dd7871cb3
hongyan <625293627@qq.com>
2022-08-09 20:53:22 +0800

    1

---
+++ delete.sh
@@ -0,0 +1,5 @@
+kubectl delete -f ./k8s/authserver.yml
+kubectl delete -f ./k8s/base-setting-service.yml
+kubectl delete -f ./k8s/gateway.yml
+kubectl delete -f ./k8s/identity-service.yml
+kubectl delete -f ./k8s/process-control-service.yml
